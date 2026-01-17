f04c12fd6127a881360392615559c78868385fe5
rutik.tade <rutiktade63@gmail.com>
2026-01-17 09:53:20 +0530

    email-service changes

--- utils/emailService.js
+++ utils/emailService.js
@@ -44,7 +44,7 @@ const sendPasswordResetEmail = async (email, resetToken) => {
     throw new Error('Email transporter not configured. Please set EMAIL_USER and EMAIL_PASS environment variables.');
   }
 
-  const resetLink = `${process.env.FRONTEND_URL || 'http://localhost:3000'}/reset-password?token=${resetToken}`;
+  const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
 
   const mailOptions = {
     from: `"${process.env.EMAIL_FROM_NAME || 'Your App'}" <${process.env.EMAIL_USER}>`,
